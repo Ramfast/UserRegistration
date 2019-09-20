d88135d6a705d47de3d8e0779cce69c75f1d030b
Mørch, Simen <k01168@norsk-tipping.no>
2019-09-20 10:45:59 +0200

    Removed unused imports

--- src/main/kotlin/com/example/userregistration/controllers/PingController.kt
+++ src/main/kotlin/com/example/userregistration/controllers/PingController.kt
@@ -1,9 +1,7 @@
 package com.example.userregistration.controllers
 
-import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.ResponseBody
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
--- src/main/kotlin/com/example/userregistration/repositories/RedisRepository.java
+++ src/main/kotlin/com/example/userregistration/repositories/RedisRepository.java
@@ -1,12 +1,10 @@
 package com.example.userregistration.repositories;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.HashOperations;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 
-@Component
+@Repository
 public class RedisRepository {
     private HashOperations hashOperations;
     private RedisTemplate redisTemplate;
