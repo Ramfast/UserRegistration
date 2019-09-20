45f0b0d11f4119134947a93d37c0bd4980b2bc3a
Mørch, Simen <k01168@norsk-tipping.no>
2019-09-20 10:48:41 +0200

    Added more validation rules to models

--- src/main/kotlin/com/example/userregistration/models/Address.kt
+++ src/main/kotlin/com/example/userregistration/models/Address.kt
@@ -1,7 +1,12 @@
 package com.example.userregistration.models
 
+import javax.validation.constraints.NotNull
+
 data class Address(
+        @NotNull
         val street: String,
+        @NotNull
         val zip: String,
+        @NotNull
         val city: String
 )
--- src/main/kotlin/com/example/userregistration/models/Personalia.kt
+++ src/main/kotlin/com/example/userregistration/models/Personalia.kt
@@ -1,7 +1,14 @@
 package com.example.userregistration.models
 
+import javax.validation.constraints.Email
+import javax.validation.constraints.NotNull
+
 data class Personalia(
+        @NotNull
+        @Email
         val email: String,
+        @NotNull
         val firstName: String,
+        @NotNull
         val lastName: String
 )
